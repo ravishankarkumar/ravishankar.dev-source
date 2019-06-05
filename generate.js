61dc6d6e923a69a6d8915231e8b42e35c46bac01
Ravi Shankar <ravishankar@UCLP1717.local>
2019-06-05 13:55:43 +0530

    updated the theme

---
+++ generate.js
@@ -0,0 +1,57 @@
+"use strict"
+
+var fs = require('fs');
+var fsex = require("fs-extra");
+var swig = require("swig");
+
+function cleanPublic() {
+  var pubDir = __dirname + "/../ravishankar.dev";
+  fsex.emptyDirSync(pubDir);
+}
+
+function copyAssets() {
+  fsex.copySync(__dirname + "/assets/", __dirname + "/../ravishankar.dev/");
+}
+
+function convertFile(path) {
+  var src = __dirname + "/src" + path;
+  var dst = __dirname + "/../ravishankar.dev" + path;
+
+  var content = fs.readFileSync(src, "utf8");
+  var pattern = "<!-- params"
+  var start =  content.indexOf(pattern);
+  var end = content.indexOf("-->", start);
+  start = start + pattern.length;
+  var pstr = content.substr(start, end - start); 
+  var params = JSON.parse(pstr);
+  var out = swig.renderFile(src, params);
+  fs.writeFileSync(dst, out, "utf8");
+}
+
+function ensurePublicPath(path) {
+  var dpath = __dirname + "/../ravishankar.dev" + path;
+  if(!fs.existsSync(dpath)) {
+    fs.mkdirSync(dpath);
+  }
+}
+
+function convertDir(path) {
+  ensurePublicPath(path);
+  var dpath = __dirname + "/src/" + path;
+  var files = fs.readdirSync(dpath);
+  files.forEach(function(f) {
+    var fp = dpath + "/" + f;
+    if(fs.lstatSync(fp).isDirectory()) {
+      convertDir(path + "/" + f);
+    } else {
+      // ignore hidden files
+      if(f.indexOf(".") != 0) {
+        convertFile(path + "/" + f);
+      }
+    }
+  });
+}
+
+cleanPublic();
+copyAssets();
+convertDir("");
